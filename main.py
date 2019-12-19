a12d6b85b90ea11f793591e32db48dd27da0f26e
LuukeLL <lucas-lamberti@web.de>
2019-12-19 19:57:52 +0100

    Added iq sample file

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+import scipy
+from matplotlib import pyplot as plt
+
 
 def main():
-    print("Hello DSP World")
+    # Source of this IQ File:
+    # https://openofdm.readthedocs.io/en/
+    wave = scipy.fromfile("IQSources/Samples.dat", dtype=scipy.int16)
+    samples = [complex(i, q) for i, q in zip(wave[::2], wave[1::2])]
 
-
+    plt.plot(samples)
+    plt.show()
 
 
 if __name__ == "__main__":
